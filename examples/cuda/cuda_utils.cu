399a37f754c7408a35fd8c2244cd072dad873490
fauberva <florian.auberval@irit.fr>
2026-01-05 13:28:58 +0100

    [examples] Refactoring CUDA examples directory layout
    Moved the utils CUDA functions to its own file
    
    Signed-off-by: fauberva <florian.auberval@irit.fr>

---
+++ examples/cuda/cuda_utils.cu
@@ -0,0 +1,91 @@
+#pragma once
+
+/*! \brief Converts a STL-like container of DataPoint to flattened arrays of positions and normals (one dimension).
+ *
+ * \tparam DataPoint The DataPoint type.
+ * \tparam PointContainer A STL-like container of DataPoint.
+ * \param points As the input, an STL-like container that contains the point position and normal.
+ * \param positionsOutput As an output, the flattened positions array.
+ * \param normalsOutput As an output, the flattened normal array.
+ */
+template<typename DataPoint, typename PointContainer>
+__host__ void pointsToFlattenedArray(
+    PointContainer & points,
+    typename DataPoint::Scalar * const positionsOutput,
+    typename DataPoint::Scalar * const normalsOutput
+) {
+    for (int i = 0; i < points.size(); ++i) {
+        const int singleDimIndex = i*DataPoint::Dim;
+        for (int d = 0; d < DataPoint::Dim; ++d)
+        {
+            positionsOutput[singleDimIndex + d] = points[i].pos()[d];
+            normalsOutput  [singleDimIndex + d] = points[i].normal()[d];
+        }
+    }
+}
+
+/*! \brief Extract a vector from a flattened array of vectors
+ *
+ * \tparam DataPoint The DataPoint type containing the VectorType and the number of Dimension.
+ * \param idx The id of the vector that needs to be extracted (the index of the vector in the flattened array is idx * Dimension).
+ * \param flattenedVectorBuffer The flattened vector array, that is of size : total number of vector * Dimension.
+ * \return The vector that was extracted from the flattened vector buffer.
+ */
+template <typename DataPoint>
+__host__ __device__ typename DataPoint::VectorType extractVectorFromFlattenedArray(
+    const int idx,
+    const typename DataPoint::Scalar * const flattenedVectorBuffer
+) {
+    using VectorType = typename DataPoint::VectorType;
+    const int singleDimIndex = idx * DataPoint::Dim;
+    VectorType v;
+
+    for (int d = 0; d < DataPoint::Dim; ++d) {
+        v.row(d) << flattenedVectorBuffer[singleDimIndex + d];
+    }
+
+    return v;
+}
+
+/*! \brief Make a DataPoint from the positions and normals flattened array
+ *
+ * \tparam DataPoint The DataPoint type containing the VectorType and the number of Dimension.
+ * \param index The index of the point that needs to be extracted from the flattened arrays.
+ * \param positions As an input, the flattened positions array.
+ * \param normals As an input, the flattened normal array.
+ */
+template<typename DataPoint>
+__host__ __device__ DataPoint makeDataPoint(
+    const int index,
+    const typename DataPoint::Scalar * const positions,
+    const typename DataPoint::Scalar * const normals
+) {
+    using VectorType = typename DataPoint::VectorType;
+
+    VectorType position = extractVectorFromFlattenedArray<DataPoint>(index, positions);
+    VectorType normal   = extractVectorFromFlattenedArray<DataPoint>(index, normals);
+
+    return DataPoint(position, normal);
+}
+
+/*! \brief Converts a flattened arrays of positions and normals (one dimension) to a STL-like container of DataPoint.
+ *
+ * \warning The point container must be of the correct sized, because `points.size()` is used to determine the number
+ * of points in the arrays.
+ *
+ * \tparam DataPoint The DataPoint type containing the VectorType and the number of Dimension.
+ * \tparam PointContainer A STL-like container of DataPoint.
+ * \param positions As an input, the flattened positions array.
+ * \param normals As an input, the flattened normal array.
+ * \param pointsOutput As an output, an STL-like container that contains the point position and normal.
+ */
+template<typename DataPoint, typename PointContainer>
+__host__ void flattenedArrayToPoints(
+    const typename DataPoint::Scalar * const positions,
+    const typename DataPoint::Scalar * const normals,
+    PointContainer & pointsOutput
+) {
+    for (int i = 0; i < pointsOutput.size(); ++i) {
+        pointsOutput[i] = makeDataPoint<DataPoint>(i, positions, normals);
+    }
+}
--- examples/cuda/ponca_fit_plane.cu
+++ examples/cuda/ponca_fit_plane.cu
@@ -8,106 +8,12 @@
 #include <Ponca/src/SpatialPartitioning/KdTree/kdTree.h>
 #include <vector>
 
-
+#include "cuda_utils.cu"
 #include "../../tests/common/testing.h"
 #include "../../tests/common/testUtils.h"
 
 
-/*!
- * \brief Converts a STL-like container of DataPoint to flattened arrays of positions and normals (one dimension).
- *
- * \tparam DataPoint The DataPoint type.
- * \tparam PointContainer A STL-like container of DataPoint.
- * \param points As the input, an STL-like container that contains the point position and normal.
- * \param positionsOutput As an output, the flattened positions array.
- * \param normalsOutput As an output, the flattened normal array.
- */
-template<typename DataPoint, typename PointContainer>
-__host__ void pointsToFlattenedArray(PointContainer & points, typename DataPoint::Scalar * positionsOutput, typename DataPoint::Scalar * normalsOutput)
-{
-    for (int i = 0; i < points.size(); ++i) {
-        const int singleDimIndex = i*DataPoint::Dim;
-        for (int d = 0; d < DataPoint::Dim; ++d)
-        {
-            positionsOutput[singleDimIndex + d] = points[i].pos()[d];
-            normalsOutput  [singleDimIndex + d] = points[i].normal()[d];
-        }
-    }
-}
-
-/*! \brief Extract a vector from a flattened array of vectors
- *
- * \tparam DataPoint Contains the Dimension of the vector and the VectorType.
- * \param idx The id of the vector that needs to be extracted (the index of the vector in the flattened array is idx * Dimension).
- * \param flattenedVectorBuffer The flattened vector array, that is of size : total number of vector * Dimension.
- * \return The vector that was extracted from the flattened vector buffer.
- */
-template <typename DataPoint>
-__host__ __device__ typename DataPoint::VectorType extractVectorFromFlattenedArray(int idx, typename DataPoint::Scalar * flattenedVectorBuffer) {
-    using VectorType = typename DataPoint::VectorType;
-    const int singleDimIndex = idx * DataPoint::Dim;
-    VectorType v;
-
-    for (int d = 0; d < DataPoint::Dim; ++d) {
-        v.row(d) << flattenedVectorBuffer[singleDimIndex + d];
-    }
-
-    return v;
-}
-
-/*!
- * \brief Make a DataPoint from the positions and normals array
- *
- * \tparam DataPoint The DataPoint type.
- * \param index The index of the point.
- * \param positions As an input, the flattened positions array.
- * \param normals As an input, the flattened normal array.
- */
-template<typename DataPoint>
-__host__ __device__ DataPoint makeDataPoint(
-    const int index,
-    const typename DataPoint::Scalar * positions,
-    const typename DataPoint::Scalar * normals
-) {
-    using VectorType = typename DataPoint::VectorType;
-
-    VectorType position = VectorType::Zero();
-    VectorType normal   = VectorType::Zero();
-
-    const int singleDimIndex = index * DataPoint::Dim;
-    for (int d = 0; d < DataPoint::Dim; ++d) {
-        position.row(d) << positions[singleDimIndex + d];
-        normal.row(d)   << normals  [singleDimIndex + d];
-    }
-
-    return DataPoint(position, normal);
-}
-
-/*!
- * \brief Converts a flattened arrays of positions and normals (one dimension) to a STL-like container of DataPoint.
- *
- * \warning The point container must be of the correct sized, because `points.size()` is used to determine the number
- * of points in the arrays.
- *
- * \tparam DataPoint The DataPoint type.
- * \tparam PointContainer A STL-like container of DataPoint.
- * \param positions As an input, the flattened positions array.
- * \param normals As an input, the flattened normal array.
- * \param pointsOutput As an output, an STL-like container that contains the point position and normal.
- */
-template<typename DataPoint, typename PointContainer>
-__host__ void flattenedArrayToPoints(
-    const typename DataPoint::Scalar * positions,
-    const typename DataPoint::Scalar * normals,
-    PointContainer & pointsOutput
-) {
-    for (int i = 0; i < pointsOutput.size(); ++i) {
-        pointsOutput[i] = makeDataPoint<DataPoint>(i, positions, normals);
-    }
-}
-
-/*!
- * \brief Computes a fit for each point of the point cloud and returns the potential result.
+/*! \brief Computes a fit for each point of the point cloud and returns the potential result.
  *
  * \tparam DataPoint The DataPoint type.
  * \tparam Fit The Fit that will be computed by the Kernel.
@@ -120,12 +26,12 @@ __host__ void flattenedArrayToPoints(
  */
 template<typename DataPoint, typename Fit, typename Scalar>
 __global__ void fitPotentialKernel(
-    const Scalar* positions,
-    const Scalar* normals,
+    const Scalar* const positions,
+    const Scalar* const normals,
     const Scalar analysisScale,
     const int nbPoints,
-    Scalar* potentialResults,
-    Scalar* gradiantResults
+    Scalar* const potentialResults,
+    Scalar* const gradiantResults
 
 ) {
     using VectorType = typename DataPoint::VectorType;
@@ -167,8 +73,16 @@ __global__ void fitPotentialKernel(
     }
 }
 
+/*! \brief Test a MeanPlaneFit on a plane using the CUDA kernel
+ *
+ * \tparam Scalar The scalar type (e.g. double, float, long double...)
+ * \tparam Dim The number of dimension that the VectorType will have.
+ * \param _bUnoriented Generates an unoriented point cloud.
+ * \param _bAddPositionNoise Determines if we add a randomly generated offset to the posion.
+ * \param _bAddNormalNoise Determines if we add a randomly generated offset to the normal.
+ */
 template<typename Scalar, int Dim>
-__host__ void testPlaneCuda(bool _bUnoriented = false, bool _bAddPositionNoise = false, bool _bAddNormalNoise = false)
+__host__ void testPlaneCuda( const bool _bUnoriented = false,  const bool _bAddPositionNoise = false,  const bool _bAddNormalNoise = false)
 {
     typedef PointPositionNormal<Scalar, Dim> DataPoint;
     typedef Ponca::DistWeightFunc<DataPoint, Ponca::SmoothWeightKernel<Scalar> > WeightSmoothFunc;
@@ -245,13 +159,10 @@ __host__ void testPlaneCuda(bool _bUnoriented = false, bool _bAddPositionNoise =
 
     for (int j = 0; j < nbPoints; ++j) {
         VectorType primGrad = extractVectorFromFlattenedArray<DataPoint>(j, gradientResults);
-
-        if(!_bAddPositionNoise) {
-            std::cout << "j:" << j << ", potential:"<< potentialResults[j] << ", ";
-            std::cout << "primitiveGradient:" << primGrad(0) << ", " << primGrad(1) << ", " << primGrad(2) << " ; " << std::endl;
-            VERIFY(std::abs(potentialResults[j]) <= epsilon);
-            VERIFY(Scalar(1.) - std::abs(primGrad.dot(direction)) <= epsilon);
-        }
+        std::cout << "j:" << j << ", potential:"<< potentialResults[j] << ", ";
+        std::cout << "primitiveGradient:" << primGrad.transpose() << " ; " << std::endl;
+        VERIFY(std::abs(potentialResults[j]) <= epsilon);
+        VERIFY(Scalar(1.) - std::abs(primGrad.dot(direction)) <= epsilon);
     }
 
     delete[] potentialResults;
